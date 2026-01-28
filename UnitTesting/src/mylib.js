33e06af0d25feb01633c4e735df4b479f599aa23
henna003 <hennakekola@gmail.com>
2026-01-28 15:59:26 +0200

    mylib.js

---
+++ UnitTesting/src/mylib.js
@@ -0,0 +1,33 @@
+
+// basic arithmetic operations
+
+// addition
+function add(a, b) {
+    return a + b;
+}
+
+//subtract
+function subtract(a, b) {
+    return a - b;
+}
+
+// multiply
+function multiply(a, b) {
+    return a * b;
+}
+
+//divide
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error("ZeroDivision");
+    }
+    return a / b;
+}
+
+// Export arithmetic functions for use in other modules
+module.exports = {
+    add, 
+    subtract,
+    multiply,
+    divide
+};
