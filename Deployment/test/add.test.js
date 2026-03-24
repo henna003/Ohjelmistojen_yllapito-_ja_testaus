ee3059692cec8da8f9ebebbb0375e2b60bc54aa0
henna003 <hennakekola@gmail.com>
2026-03-24 20:31:07 +0200

    coveralls

--- Deployment/test/add.test.js
+++ Deployment/test/add.test.js
@@ -10,3 +10,20 @@ describe('add function', () => {
     expect(add(5, 0)).to.equal(5);
     });
 });
+
+// import proxyquire from 'proxyquire';
+// import { expect } from 'chai';
+
+// const add = proxyquire('../src/add.js', {'./.internal/createMathOperation.js': (fn, initial) => fn
+// });
+
+// describe('add function', () => {
+//     it('adds two positive numbers', () => {
+//     expect(add(6, 4)).to.equal(10);
+//     });
+
+//     it('adds with zero', () => {
+//     expect(add(5, 0)).to.equal(5);
+//     });
+// });
+
